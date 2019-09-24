4c977334e001849caeacdd3d83625f249223af6b
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-23 21:12:17 -0300

    Primeiro teste

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import { Container } from './styles';
 
 export default function TechList() {
-  return <div />;
+  const [techs, setTechs] = useState([]);
+  function handleAddTech() {
+    setTechs([...techs, "Node.js"]);
+  }
+  return (
+    <div>
+      <ul data-testid="tech-list">
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddTech}>Adicionar</button>
+    </div>
+  );
 }
