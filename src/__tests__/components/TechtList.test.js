773ffb2e07d63c3014bca0bd71eb354ee2e7e4b7
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-24 07:53:24 -0300

    Mock do useSelector

--- src/__tests__/components/TechtList.test.js
+++ src/__tests__/components/TechtList.test.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { useSelector } from "react-redux";
 
 import TechList from "~/components/TechList";
 
+jest.mock("react-redux");
+
 describe("TechList component", () => {
-  it("");
+  it("should render tech list", () => {
+    useSelector.mockImplementation(cb =>
+      cb({
+        techs: ["Node.js", "ReactJS"]
+      })
+    );
+
+    const { getByTestId, getByText, debug } = render(<TechList />);
+
+    debug();
+
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByTestId("tech-list")).toContainElement(getByText("ReactJS"));
+  });
 });
