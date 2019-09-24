35fb424a82bfb15aec846d1defd520ceca8d33c6
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-24 10:45:16 -0300

    testando sagas

---
+++ src/store/modules/techs/sagas.js
@@ -0,0 +1,13 @@
+import { call, put } from "redux-saga/effects";
+import api from "../../../services/api";
+
+import * as Actions from "./actions";
+
+export function* getTechs() {
+  try {
+    const response = yield call(api.get, "techs");
+    yield put(Actions.getTechSuccess(response.data));
+  } catch (err) {
+    yield put(Actions.getTechsFailure());
+  }
+}
